93a85cf5717eab205526810c24a2c106c6e913da
Robert Cato <saiwolf@swmnu.net>
2019-06-23 17:02:55 -0500

    Initial Commit

---
+++ apiInClientwithJWT/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace apiInClientwithJWT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var hostingEnvironment = services.GetService<IHostingEnvironment>();
+                Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console()
+                        .CreateLogger();
+            }
+            try
+            {
+                Log.Information("Starting Web Host");
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal($"Host terminated unexpectedly!\n{ex.Message}");
+                host.Dispose();
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+    }
+}
